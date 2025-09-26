375b71e2f81a0a59ad5063bf597da16cd03349c6
rezashokrzad <rezashokrzad@gmail.com>
2025-09-26 14:00:18 +0200

    update

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,13 +1,15 @@
 import { MetadataRoute } from "next";
 
+const BASE = 'https://rezashokrzad.github.io/my_next_app'
 
 export default function sitemap(): MetadataRoute.Sitemap {
 const base = process.env.NEXT_PUBLIC_SITE_URL ?? "https://example.com";
 return [
-{ url: base, priority: 1 },
-{ url: base + "/blog", priority: 0.8 },
-{ url: base + "/shop", priority: 0.8 },
-{ url: base + "/forum", priority: 0.6 },
-{ url: base + "/about", priority: 0.5 },
+    { url: `${BASE}/`, changeFrequency: 'weekly', priority: 1 },
+    { url: base, priority: 1 },
+    { url: base + "/blog", priority: 0.8 },
+    { url: base + "/shop", priority: 0.8 },
+    { url: base + "/forum", priority: 0.6 },
+    { url: base + "/about", priority: 0.5 },
 ];
 }
