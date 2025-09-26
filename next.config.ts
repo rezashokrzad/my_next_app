6d8fe40cf2572d81710426bb35f4c147e65055c6
rezashokrzad <rezashokrzad@gmail.com>
2025-09-26 13:17:18 +0200

    update

--- next.config.ts
+++ next.config.ts
@@ -5,3 +5,17 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+// next.config.js
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+const repoName = 'my_next_app'
+
+module.exports = {
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: isProd ? `/${repoName}` : undefined,
+  assetPrefix: isProd ? `/${repoName}/` : undefined,
+  // trailingSlash: true, // uncomment if you hit relative-path issues
+}
+
